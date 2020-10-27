c35e19178decfed06cf5b8218250dd4a5188fe13
Russel Simmons <russ@rsimmons.org>
2020-10-27 16:23:41 -0700

    Deploy batch code to /opt/massif/batch

--- ops/deploy-batch.sh
+++ ops/deploy-batch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 cd $(git rev-parse --show-toplevel)
-PREFIX=massif-$(git rev-parse HEAD)
-git archive --format=tar.gz --prefix=$PREFIX/ HEAD:batch | ssh ubuntu@$1 "(cd /tmp && tar xfz - && ln -s $PREFIX massif)"
+PREFIX=batch-$(git rev-parse HEAD)
+git archive --format=tar.gz --prefix=$PREFIX/ HEAD:batch | ssh ubuntu@$1 "(cd /opt/massif && tar xfz - && ln -s $PREFIX batch)"
