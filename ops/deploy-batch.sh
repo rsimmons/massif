b69626e26c0d59b4aebbe3a0f7a34c1dca295b2b
Russel Simmons <russ@rsimmons.org>
2020-10-27 16:40:25 -0700

    Fix symlink creation when pushing batch scripts

--- ops/deploy-batch.sh
+++ ops/deploy-batch.sh
@@ -2,4 +2,4 @@
 set -e
 cd $(git rev-parse --show-toplevel)
 PREFIX=batch-$(git rev-parse HEAD)
-git archive --format=tar.gz --prefix=$PREFIX/ HEAD:batch | ssh ubuntu@$1 "(cd /opt/massif && tar xfz - && ln -s $PREFIX batch)"
+git archive --format=tar.gz --prefix=$PREFIX/ HEAD:batch | ssh ubuntu@$1 "(cd /opt/massif && tar xfz - && ln -sfT $PREFIX batch)"
