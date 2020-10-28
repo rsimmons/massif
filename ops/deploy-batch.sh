7b9952c032f6b96b9c5a8ea7b4cae7989bcede95
Russel Simmons <russ@rsimmons.org>
2020-10-28 09:22:17 -0700

    Make scripts set up python env for batch scripts

--- ops/deploy-batch.sh
+++ ops/deploy-batch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+
+if [ -z "$1" ]; then
+  echo "need hostname"
+  exit 1
+fi
+
 cd $(git rev-parse --show-toplevel)
 PREFIX=batch-$(git rev-parse HEAD)
-git archive --format=tar.gz --prefix=$PREFIX/ HEAD:batch | ssh ubuntu@$1 "(cd /opt/massif && tar xfz - && ln -sfT $PREFIX batch)"
+git archive --format=tar.gz --prefix=$PREFIX/ HEAD:batch | ssh ubuntu@$1 "(cd /opt/massif && tar xfz - && ln -sfT $PREFIX batch && python3 -m venv /opt/massif/batch-env && /opt/massif/batch-env/bin/pip install -r /opt/massif/batch/requirements.txt)"
