7ac04d58a5ae64613ab0273cb5099bff56a6f178
Russel Simmons <russ@rsimmons.org>
2020-10-29 12:17:24 -0700

    Fix issue with no search results

--- web/application.py
+++ web/application.py
@@ -12,6 +12,8 @@ def search():
     query = request.args.get('q', '')
     print('query is', query)
 
+    results = {}
+
     main_resp = requests.get(f'http://localhost:9200/ja_sent_main/_search', json={
         'query': {
             'match_phrase': {
@@ -30,20 +32,23 @@ def search():
 
     hitcount_value =  main_resp_body['hits']['total']['value']
     if main_resp_body['hits']['total']['relation'] == 'eq':
-        hitcount_str = f'{hitcount_value}'
+        results['count_str'] = f'{hitcount_value}'
     elif main_resp_body['hits']['total']['relation'] == 'gte':
-        hitcount_str = f'>{hitcount_value}'
+        results['count_str'] = f'>{hitcount_value}'
     else:
         assert False
 
     meta_ids = [hit['_source']['mid'] for hit in main_resp_body['hits']['hits']]
 
-    meta_resp = requests.get(f'http://localhost:9200/ja_sent_meta/_mget', json={'ids': meta_ids})
-    meta_resp.raise_for_status()
-    meta_resp_body = meta_resp.json()
-    meta_map = {doc['_id']: doc['_source'] for doc in meta_resp_body['docs']}
+    if meta_ids:
+        meta_resp = requests.get(f'http://localhost:9200/ja_sent_meta/_mget', json={'ids': meta_ids})
+        meta_resp.raise_for_status()
+        meta_resp_body = meta_resp.json()
+        meta_map = {doc['_id']: doc['_source'] for doc in meta_resp_body['docs']}
+    else:
+        meta_map = {}
 
-    hits = []
+    results_list = []
     for hit in main_resp_body['hits']['hits']:
         xhit = {
             'markup': hit['highlight']['text'][0].replace('\n', '<br>')
@@ -51,9 +56,10 @@ def search():
         mdata = meta_map.get(hit['_source']['mid'])
         if mdata:
             xhit['title'] = mdata['title']
-        hits.append(xhit)
+        results_list.append(xhit)
+    results['list'] = results_list
 
-    return render_template('index.html', query=query, hitcount=hitcount_str, hits=hits)
+    return render_template('index.html', query=query, results=results)
 
 application = app # for EB
 
