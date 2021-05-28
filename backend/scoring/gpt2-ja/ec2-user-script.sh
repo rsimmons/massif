35d6e75a1ab48ec5c9a356e51a529022d217cb76
Russel Simmons <russ@rsimmons.org>
2021-05-28 16:28:49 -0700

    Add user script for ml ec2 instances

---
+++ backend/scoring/gpt2-ja/ec2-user-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir /opt/massif
+chown ubuntu:ubuntu /opt/massif
+chmod 755 /opt/massif
+sudo -i -u ubuntu bash << EOF
+cd /opt/massif
+source activate tensorflow_p37
+pip install jaconv
+git clone https://github.com/tanreinama/gpt2-japanese
+cd gpt2-japanese
+wget https://www.nama.ne.jp/models/gpt2ja-medium.tar.bz2
+tar xvfj gpt2ja-medium.tar.bz2
+curl -O https://raw.githubusercontent.com/rsimmons/massif/master/backend/scoring/gpt2-ja/score.py
+EOF
