4c926510ea838963c0363aa1db0dcab62921c4bc
Russel Simmons <russ@rsimmons.org>
2021-06-13 14:58:18 -0700

    Show fragment source info, remove chunk code

--- web/application.py
+++ web/application.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 
 from flask import Flask, request, render_template, redirect, url_for
 import requests
@@ -13,9 +14,8 @@ ES_BASE_URL = f'http://{ES_HOST}:9200'
 RESULTS_PER_PAGE = 25
 FRAGMENT_RESULTS_PER_PAGE = 100
 
-CHUNK_INDEX = 'chunk_ja'
-META_INDEX = 'meta_ja'
 FRAGMENT_INDEX = 'fragment_ja'
+SOURCE_INDEX = 'source_ja'
 
 @app.before_request
 def before_request():
@@ -47,128 +47,16 @@ def format_time(t):
     return result
 
 @app.route('/ja/search')
-def ja_search():
-    query = request.args.get('q', '')
-    phrases = query.split()
-    print('query phrases are', phrases)
-
-    results = {}
-
-    main_resp = requests.get(f'{ES_BASE_URL}/{CHUNK_INDEX}/_search', json={
-        'query': {
-            'bool': {
-                'must': [{'match_phrase': {'html': phrase}} for phrase in phrases]
-            }
-        },
-        'highlight': {
-            'type': 'unified',
-            'fields': {
-                'html': {
-                    'number_of_fragments': 0, # forces it to return entire field
-                },
-            },
-        },
-        'size': RESULTS_PER_PAGE,
-    })
-    main_resp.raise_for_status()
-
-    main_resp_body = main_resp.json()
-
-    hitcount_value = main_resp_body['hits']['total']['value']
-    if main_resp_body['hits']['total']['relation'] == 'eq':
-        hitcount_qual = ''
-    elif main_resp_body['hits']['total']['relation'] == 'gte':
-        hitcount_qual = '>'
-    else:
-        assert False
-    hitcount_str = hitcount_qual + str(hitcount_value)
-    if hitcount_value > RESULTS_PER_PAGE:
-        results['count_str'] = f'showing {RESULTS_PER_PAGE} of {hitcount_str} results'
-    else:
-        results['count_str'] = f'showing {hitcount_str} results'
-
-    meta_idx_ids = []
-    for hit in main_resp_body['hits']['hits']:
-        cidx = hit['_index']
-        assert cidx.startswith('chunk_')
-        midx = 'meta_' + cidx[len('chunk_'):]
-        meta_idx_ids.append((midx, hit['_source']['mid']))
-
-    if meta_idx_ids:
-        meta_resp = requests.get(f'{ES_BASE_URL}/_mget', json={'docs': [{'_index': idx, '_id': did} for (idx, did) in meta_idx_ids]})
-        meta_resp.raise_for_status()
-        meta_resp_body = meta_resp.json()
-        meta_map = {doc['_id']: doc['_source'] for doc in meta_resp_body['docs']}
-    else:
-        meta_map = {}
-
-    results_list = []
-    for hit in main_resp_body['hits']['hits']:
-        xhit = {}
-
-        mdata = meta_map.get(hit['_source']['mid'], {})
-
-        xhit['title'] = mdata['title']
-        if 'published' in mdata:
-            xhit['published'] = mdata['published']
-
-        if 'url' in mdata:
-            xhit['url'] = mdata['url']
-
-        chunk_tags = hit['_source'].get('tags', [])
-        xhit['tags'] = []
-        for t, trans in [('novel', '小説'), ('drama', 'ドラマ')]:
-            if t in chunk_tags:
-                xhit['tags'].append(trans)
-
-        hit_html = hit['highlight']['html'][0]
-        soup = BeautifulSoup(hit_html, 'html.parser')
-
-        min_time = None
-        max_time = None
-        for p in soup.find_all('p'):
-            if p.get('a') and mdata.get('url'):
-                p_url = mdata['url'] + '#' + p['a']
-                del p['a']
-
-                link_icon_tag = soup.new_tag('img')
-                link_icon_tag['class'] = 'link-icon'
-                link_icon_tag['src'] = url_for('static', filename='link.svg')
-                p.append(link_icon_tag)
-
-                a_tag = soup.new_tag('a', href=p_url)
-                a_tag['class'] = 'source-anchor-link'
-                # p.wrap(a_tag) # this made the entire text linked. keep it in case we change our mind
-                link_icon_tag.wrap(a_tag)
-
-            for attr in ['t0', 't1']:
-                if p.get(attr):
-                    min_time = min(float(p[attr]), min_time if (min_time is not None) else float('inf'))
-                    max_time = max(float(p[attr]), min_time if (min_time is not None) else 0)
-                    del p[attr]
-        xhit['markup'] = str(soup)
-
-        if (min_time is not None) and (max_time is not None):
-            time_range_str = format_time(min_time) + '-' + format_time(max_time)
-            xhit['time_range'] = time_range_str
-
-        results_list.append(xhit)
-
-    results['list'] = results_list
-
-    return render_template('index.html', query=query, results=results)
-
-@app.route('/ja/fsearch')
 def ja_fsearch():
     query = request.args.get('q', '')
-    if not query:
-        return render_template('findex.html')
 
+    # PARSE QUERY
     phrases = query.split()
     print('query phrases are', phrases)
 
     results = {}
 
+    # SEARCH FRAGMENTS
     t0 = time.time()
     main_resp = requests.get(f'{ES_BASE_URL}/{FRAGMENT_INDEX}/_search', json={
         'query': {
@@ -194,6 +82,7 @@ def ja_fsearch():
     main_resp.raise_for_status()
     print(f'took {dt}s')
 
+    # FORMAT RESULT COUNT
     main_resp_body = main_resp.json()
     hitcount_value = main_resp_body['hits']['total']['value']
 
@@ -206,22 +95,64 @@ def ja_fsearch():
         assert False
     hitcount_str = hitcount_qual + str(hitcount_value)
     if hitcount_value > FRAGMENT_RESULTS_PER_PAGE:
-        results['count_str'] = f'showing {FRAGMENT_RESULTS_PER_PAGE} of {hitcount_str} results'
+        results['count_str'] = f'showing first {FRAGMENT_RESULTS_PER_PAGE} of {hitcount_str} unique matching sentences'
     else:
-        results['count_str'] = f'showing {hitcount_str} results'
+        results['count_str'] = f'showing {hitcount_str}  unique matching sentences'
 
-    results_list = []
+    # FIGURE OUT SOURCE IDS TO FETCH
+    source_infos = [] # list of {total_hits, source_id, loc}
     for hit in main_resp_body['hits']['hits']:
+        total_count = 0
+        combined_sample_hits = []
+        for (tag_set_str, tag_set_info) in hit['_source']['hits'].items():
+            total_count += tag_set_info['count']
+            combined_sample_hits.extend(tag_set_info['sample'])
+        chosen_hit = random.choice(combined_sample_hits)
+
+        source_infos.append({
+            'total_hits': total_count,
+            'source_id': chosen_hit['source_id'],
+            'loc': chosen_hit['loc'],
+        })
+
+    # FETCH SOURCE RECORDS
+    if source_infos:
+        unique_source_ids = set(s['source_id'] for s in source_infos)
+        source_resp = requests.get(f'{ES_BASE_URL}/_mget', json={'docs': [{'_index': SOURCE_INDEX, '_id': sid} for sid in unique_source_ids]})
+        source_resp.raise_for_status()
+        source_resp_body = source_resp.json()
+        source_map = {doc['_id']: doc['_source'] for doc in source_resp_body['docs']}
+    else:
+        # empty query doesn't work IIRC
+        source_map = {}
+
+    # PREPARE RESULT LIST FOR TEMPLATE
+    results_list = []
+    assert len(main_resp_body['hits']['hits']) == len(source_infos) # sanity check
+    for (hit, source_info) in zip(main_resp_body['hits']['hits'], source_infos):
         xhit = {}
 
         hit_html = hit['highlight']['text'][0]
-
         xhit['markup'] = str(hit_html)
+
+        source_record = source_map[str(source_info['source_id'])]
+        xhit['title'] = source_record['title']
+        if 'published' in source_record:
+            xhit['published'] = source_record['published']
+        if 'url' in source_record:
+            xhit['url'] = source_record['url']
+        xhit['other_count'] = source_info['total_hits'] - 1
+        # Uncomment this to add back in tags display
+        # source_tags = source_record['tags']
+        # xhit['tags'] = []
+        # for t, trans in [('novel', '小説'), ('drama', 'ドラマ')]:
+        #     if t in source_tags:
+        #         xhit['tags'].append(trans)
 
         results_list.append(xhit)
 
     results['list'] = results_list
 
-    return render_template('findex.html', query=query, results=results)
+    return render_template('index.html', query=query, results=results)
 
 application = app # for EB
