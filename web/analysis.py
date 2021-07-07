824dced89e700c087ac27aaa7e44c743c053d631
Russel Simmons <russ@rsimmons.org>
2021-07-07 16:40:56 -0700

    Improve furigana algorithm

--- web/analysis.py
+++ web/analysis.py
@@ -3,10 +3,7 @@ import re
 from collections import Counter
 
 from sudachipy import tokenizer, dictionary
-from diff_match_patch import diff_match_patch
 import jaconv
-
-dmp = diff_match_patch()
 
 tokenizer_obj = dictionary.Dictionary().create()
 
@@ -18,6 +15,13 @@ NUMERALS_RE = re.compile('[0-9０-９]')
 def has_any_numerals(s):
     return bool(NUMERALS_RE.search(s))
 
+def char_could_have_reading(c):
+    return has_any_kanji(c) or has_any_numerals(c) or c in '々〇カケヵヶぁぃぅぇぉァィゥェォ'
+
+# KATAKANA_RE = re.compile(r'[ァ-ー]')
+# def has_any_katakana(c):
+#     return bool(KATAKANA_RE.search(s))
+
 def ignore_morpheme(m):
     return m.part_of_speech() in ['補助記号', '空白']
 
@@ -28,8 +32,83 @@ def ja_get_text_normal_counts(text):
             result[m.normalized_form()] += 1
     return result
 
+# returns text with furigana in "Anki format", readings converted to hiragana
+def ja_match_furigana(kanji_text, kana_text, not_start):
+    # We use a non-recursive DFS, storing an explicit stack
+    len_kanji_text = len(kanji_text)
+    len_kana_text = len(kana_text)
+    stack = [{
+        'kanji_idx': 0,
+        'kana_idx': 0,
+        'result': '',
+        'accum_kana': '',
+    }]
+
+    while stack:
+        top = stack.pop()
+        kanji_idx = top['kanji_idx']
+        kana_idx = top['kana_idx']
+        result = top['result']
+        accum_kana = top['accum_kana']
+        if kanji_idx == len_kanji_text:
+            if kana_idx == len_kana_text:
+                # we found a match!
+                assert not accum_kana
+                return result
+            else:
+                # dead end
+                pass
+        else:
+            if kana_idx == len_kana_text:
+                # dead end
+                pass
+            else:
+                kanji_char = kanji_text[kanji_idx]
+                kana_char = kana_text[kana_idx]
+                norm_kanji_char = jaconv.hira2kata(kanji_char)
+                norm_kana_char = jaconv.hira2kata(kana_char)
+                if (norm_kanji_char == norm_kana_char) or \
+                  ((norm_kanji_char == 'ハ') and (norm_kana_char == 'ワ')) or \
+                  ((norm_kanji_char == 'ヂ') and (norm_kana_char == 'ジ')) or \
+                  ((norm_kanji_char == 'ヅ') and (norm_kana_char == 'ズ')):
+                    assert not accum_kana
+                    stack.append({
+                        'kanji_idx': kanji_idx + 1,
+                        'kana_idx': kana_idx + 1,
+                        'result': result + kanji_char,
+                        'accum_kana': '',
+                    })
+                elif char_could_have_reading(kanji_char):
+                    # we didn't check if kana_char is actually kana (as opposed to punctuation)
+                    # because analyzer should make those into separate morphemes
+
+                    # find the next index after the end of the kanji run
+                    after_run_idx = kanji_idx + 1
+                    while (after_run_idx < len_kanji_text) and char_could_have_reading(kanji_text[after_run_idx]):
+                        after_run_idx += 1
+
+                    # NOTE: The order that we push onto the stack is extremely important to not have horrible performance
+
+                    stack.append({
+                        'kanji_idx': kanji_idx,
+                        'kana_idx': kana_idx + 1,
+                        'result': result,
+                        'accum_kana': accum_kana + kana_text[kana_idx],
+                    })
+
+                    stack.append({
+                        'kanji_idx': after_run_idx,
+                        'kana_idx': kana_idx + 1,
+                        'result': result + (' ' if (not_start or result) else '') + kanji_text[kanji_idx:after_run_idx] + '[' + jaconv.kata2hira(accum_kana + kana_text[kana_idx]) + ']',
+                        'accum_kana': '',
+                    })
+
+    # print('NO MATCHES', repr(kanji_text), repr(kana_text))
+    # We occasionally don't match in weird cases, but that's OK
+    return kanji_text
+
 def ja_get_morphemes_reading(morphemes):
-    result = ''
+    result_pieces = []
 
     for m in morphemes:
         pos = m.part_of_speech()
@@ -39,30 +118,11 @@ def ja_get_morphemes_reading(morphemes):
             # or there aren't any kanji or numerals,
             # then just copy the surface form.
             # This avoids weird bugs where the reading doesn't really match the surface.
-            result += surface
+            result_pieces.append(surface)
         else:
-            kata_surface = jaconv.hira2kata(surface)
-            diff = dmp.diff_main(kata_surface, m.reading_form())
-            # print('DIFF', repr(kata_surface), repr(m.reading_form()), diff)
-            i = 0
-            cidx = 0
-            while i < len(diff):
-                op, t = diff[i]
-                if op == 0:
-                    result += surface[cidx:][:len(t)]
-                    i += 1
-                    cidx += len(t)
-                else:
-                    assert op == -1
-                    assert (i + 1) < len(diff)
-                    next_op, next_t = diff[i+1]
-                    assert next_op == 1
+            result_pieces.append(ja_match_furigana(surface, m.reading_form(), bool(result_pieces)))
 
-                    result += (' ' if result else '') + t + '[' + jaconv.kata2hira(next_t) + ']'
-                    i += 2
-                    cidx += len(t)
-
-    return result
+    return ''.join(result_pieces)
 
 def ja_get_text_normals_and_reading(text):
     normals_set = set()
@@ -77,8 +137,7 @@ def ja_get_text_normals_and_reading(text):
         reading = ja_get_morphemes_reading(morphemes)
     except:
         print('ERROR getting reading of', repr(text), file=sys.stderr)
-        # raise
-        reading = '' # TODO: revisit
+        raise
 
     return normals_set, reading
 
@@ -89,6 +148,7 @@ if __name__ == '__main__':
         ('foo', 'foo'),
         ('そうです。', 'そうです。'),
         ('様々', '様々[さまざま]'),
+        ('可愛い', '可愛[かわい]い'),
         ('繰り返す', '繰[く]り 返[かえ]す'),
         ('飛ばねぇ豚はただの豚だ', '飛[と]ばねぇ 豚[ぶた]はただの 豚[ぶた]だ'),
         ('ぶっ殺した', 'ぶっ 殺[ころ]した'),
@@ -97,15 +157,21 @@ if __name__ == '__main__':
         ('えええええええええええーーーーーーーーーーーーーっ！', 'えええええええええええーーーーーーーーーーーーーっ！'),
         ('１つ', '１[ひと]つ'),
         ('２人', '２人[ふたり]'),
-        ('言い聞かせる', '言[い]い 聞[き]かせる'),
-        ('聞き込みをしているうちに、', '聞[き]き　込[こ]みをしているうちに、'),
-        ('どうやら相当の思い入れがあったらしい。', 'どうやら　相当[そうとう]の　思[おも]い　入[い]れがあったらしい。'),
-        ('何か喋っているようだが内容までは聞き取れない。', '何[なに]か 喋[しゃべ]っているようだが 内容[ないよう]までは 聞[き]き 取[と]れない。'),
+        ('五〇歳', '五〇[ごれい] 歳[さい]'),
+        ('ここ一ヶ月', 'ここ 一[いち] ヶ月[かげつ]'), # we would prefer いっかげつ of course..
+        ('鏡は無ぇみてぇだなァ', '鏡[かがみ]は 無ぇ[ねえ]みてぇだなァ'),
+        ('或は', '或[あるい]は'),
+        ('小ぢんまり', '小[こ]ぢんまり'),
 
+        # the following failed with the old algorithm based on diff_match_patch
+        ('言い聞かせる', '言[い]い 聞[き]かせる'),
+        ('聞き込みをしているうちに、', '聞[き]き 込[こ]みをしているうちに、'),
+        ('どうやら相当の思い入れがあったらしい。', 'どうやら 相当[そうとう]の 思[おも]い 入[い]れがあったらしい。'),
+        ('何か喋っているようだが内容までは聞き取れない。', '何[なん]か 喋[しゃべ]っているようだが 内容[ないよう]までは 聞[き]き 取[と]れない。'),
         # ('差し出がましい', '差[さ]し出[で]がましい'), # Sudachi currently gets this wrong, not our fault
     ]
     for frag, target_reading in TEST_READING_FRAGMENTS:
         print('testing', repr(frag))
         normals_set, reading = ja_get_text_normals_and_reading(frag)
-        assert reading == target_reading, repr(reading)
+        assert reading == target_reading, (repr(reading) + ' vs ' + repr(target_reading))
     print('ALL GOOD')
